554e17ceb47cd1b615c51c7a55512ae37e6ef75c
App Academy Student <student@appacademy.io>
2013-03-26 14:12:19 -0700

    Implemented Board class.

--- lib/board.rb
+++ lib/board.rb
@@ -1,3 +1,130 @@
 class Board
 
+  def self.make_start_grid
+    grid = Array.new(8) {Array.new(8, nil)}
+
+    grid = place_pieces(grid, [[3,3], [4,4]], :white)
+    grid = place_pieces(grid, [[3,4], [4,3]], :black)
+  end
+
+  def self.place_pieces(grid, positions, color)
+    positions.each do |pos|
+      i, j = pos
+      grid[i][j] = Piece.new(color)
+    end
+    grid
+  end
+
+  attr_accessor :grid
+
+  def initialize(grid = nil)
+    @grid = grid
+    @grid = Board.make_start_grid if grid.nil?
+  end
+
+  def [](position)
+    i, j = position
+    grid[i][j]
+  end
+
+  def []=(position, new_value)
+    i, j = position
+    grid[i][j] = new_value
+  end
+
+  def on_board?(position)
+    i, j = position
+    (0...8).include?(i) && (0...8).include?(j)
+  end
+
+  def valid_moves?(color)
+    (0...8).each do |i|
+      (0...8).each do |j|
+        position = [i,j]
+
+        return true if valid_lines(position, color)
+      end
+    end
+    false
+  end
+
+  def count_pieces(color)
+    counter = 0
+
+    grid.each do |row|
+      row.each do |square|
+        next if square.nil?
+        counter += 1 if square.color == color
+      end
+    end
+
+    counter
+  end
+
+  def place_piece(position, color)
+    lines = valid_lines(position, color)
+
+    raise "No valid lines" unless lines
+
+    self[position] = Piece.new(color)
+
+    color_lines(lines, color)
+  end
+
+  def line_builder(origin, delta, color)
+    raise "Origin not empty" unless self[origin].nil?
+    line = [origin]
+    current_position = origin
+
+    while true
+      current_position = step_by_delta(current_position, delta)
+
+      if !on_board?(current_position)
+        return nil
+      elsif self[current_position].nil?
+        return nil
+      elsif self[current_position].color == color && line.count > 1
+        line << current_position
+        return line
+      elsif self[current_position].color != color
+        line << current_position
+      else
+        return nil
+      end
+    end
+  end
+
+  def valid_lines(origin, color)
+    return nil unless on_board?(origin)
+    return nil unless self[origin].nil?
+
+    dir_deltas = [0, 1, -1].product([0, 1, -1])
+    dir_deltas.delete([0, 0])
+
+    lines = []
+
+    dir_deltas.each do |delta|
+      line = line_builder(origin, delta, color)
+      lines << line unless line.nil?
+    end
+
+    return nil if lines.empty?
+    lines
+  end
+
+  private
+
+  def step_by_delta(position, delta)
+    i, j = position
+    di, dj = delta
+    [i + di, j + dj]
+  end
+
+  def color_lines(lines, color)
+    lines.each do |line|
+      line.each do |position|
+        self[position].color = color
+      end
+    end
+  end
 end
--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -5,49 +5,171 @@ require 'piece'
 describe Board do
 
   describe "::make_start_grid" do
+    subject(:grid) { Board.make_start_grid }
     it "returns an 8x8 grid" do
-      board.grid.count.should == 8
-      board.grid.each do |row|
+      grid.count.should == 8
+      grid.each do |row|
         row.count.should == 8
       end
     end
 
     it "grid has starting pieces in correct location" do
-      board.grid.each_with_index do |rows, i|
+      WHITES = [[3,3], [4,4]]
+      BLACKS = [[3,4], [4,3]]
+
+      grid.each_with_index do |rows, i|
+        rows.each_with_index do |square, j|
+          square.color.should == :white if WHITES.include?([i,j])
+          square.color.should == :black if BLACKS.include?([i,j])
+          square.should be_nil unless (WHITES + BLACKS).include?([i,j])
+        end
+      end
     end
   end
 
   describe "#new" do
-    it "takes a grid and sets board.grid accordingly"
+    let(:grid) { Board.make_start_grid }
+    subject(:board) { Board.new }
+
+    it "defaults to start grid"
   end
 
   describe "#grid" do
-    it "returns the grid"
-  end
+    let(:grid) { Board.make_start_grid }
+    subject(:board) { Board.new(grid) }
+    let(:position) { [3,3] }
 
-  describe "#valid_lines" do
-    it "checks that specified origin is empty"
-    it "checks that the origin is on the board"
-    it "returns all possible lines for example grid and origin"
-    it "calls #line_builder for each delta"
+    it "returns the grid" do
+      board.grid.should == grid
+    end
+
+    it "should override [] method to lookup grid pos" do
+      board[position].should == board.grid[position[0]][position[1]]
+    end
   end
 
   describe "#line_builder" do
-    it "returns line when given valid origin, color, and delta"
-    it "returned line is an array of positions"
-    it "returned line starts with origin"
-    it "returned line ends in a piece of specified color"
-    it "returned line has middle pieces that are opposite of specified color"
-    it "returns nil if there is no valid line from origin for given delta"
+    subject(:board) { Board.new }
+    let(:origin) { [2,3] }
+    let(:bad_origin) { [3,3] }
+    let(:color) { :black }
+    let(:delta) { [1,0] }
+    let(:bad_delta) { [1,1] }
+
+    it "raises error if origin not nil" do
+      expect do
+         board.line_builder(bad_origin, delta, color)
+       end.to raise_error("Origin not empty")
+    end
+
+    it "returns line from valid origin, delta, and color" do
+      board.line_builder(origin, delta, color).should == [[2,3],[3,3],[4,3]]
+    end
+
+    it "returns end piece of specified color" do
+      position = board.line_builder(origin, delta, color).last
+      board[position].color.should == color
+    end
+
+    it "returns middle pieces of opposite color" do
+      middle = board.line_builder(origin, delta, color)[1]
+      board[middle].color.should_not == color
+    end
+
+    it "returns nil when no valid line" do
+      board.line_builder(origin, bad_delta, color).should be_nil
+    end
+  end
+
+  describe "#valid_lines" do
+    subject(:board) { Board.new }
+    let(:empty_position) { [2,3] }
+    let(:full_position) { [3,3] }
+    let(:off_position) { [9,3] }
+    let(:color) { :black }
+
+    it "returns nil if specified origin has piece" do
+      board.valid_lines(full_position, color).should be_nil
+    end
+
+    it "returns nil if origin is off board" do
+      board.valid_lines(off_position, color).should be_nil
+    end
+
+    it "calls #line_builder for each delta"
+
+    it "returns all possible lines for example grid and origin" do
+      board.valid_lines(empty_position, color).should == [[[2,3],[3,3],[4,3]]]
+    end
   end
 
   describe "#place_piece" do
-    it "checks to see if the specified location is empty"
-    it "places a piece of the specified color at specified location"
-    it "flips the middle pieces for all valid lines for specified location"
+    subject(:board) { Board.new }
+    let(:empty_position) { [2,3] }
+    let(:full_position) { [3,3] }
+    let(:color) { :black }
+    let(:line) { [[2,3], [3,3], [4,3]] }
+
+    it "raises error if there are no valid lines" do
+      expect do
+        board.place_piece(full_position, color).should
+      end.to raise_error("No valid lines")
+    end
+
+    it "places a piece at location" do
+      board.place_piece(empty_position, color)
+      board[empty_position].should_not be_nil
+    end
+
+    it "placed piece is correct color" do
+      board.place_piece(empty_position, color)
+      board[empty_position].color.should == color
+    end
+
+    it "line for specified location is correct color" do
+      board.place_piece(empty_position, color)
+      line.each do |position|
+        board[position].color.should == color
+      end
+    end
   end
 
-  describe "#set_line_color" do
-    it "sets all pieces in a given line to a specified color"
+  describe "#valid_moves?" do
+    subject(:board) do
+      grid = Array.new(8) {Array.new(8, nil)}
+      grid = Board.place_pieces(grid, [[3,6]], :black)
+      grid = Board.place_pieces(grid, [[3,7]], :white)
+      Board.new(grid)
+     end
+
+    let(:no_moves_color) { :black }
+    let(:has_moves_color) { :white }
+
+    it "returns true if player has a valid move" do
+      board.valid_moves?(has_moves_color).should be_true
+    end
+
+    it "returns false if player has no valid move" do
+      board.valid_moves?(no_moves_color).should be_false
+    end
+  end
+
+  describe "count_pieces" do
+    let(:color) { :black }
+    let(:no_pieces_color) { :white }
+    subject (:board) do
+      grid = Array.new(8) {Array.new(8, nil)}
+      positions = (0...8).to_a.map { |i| [i,i] }
+      grid = Board.place_pieces(grid, positions, color)
+      board = Board.new(grid)
+    end
+
+    it "counts pieces of certain color" do
+      board.count_pieces(color).should == 8
+    end
+
+    it "returns zero if no pieces matching color" do
+      board.count_pieces(no_pieces_color).should == 0
+    end
   end
 end
